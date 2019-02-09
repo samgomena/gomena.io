05bc1d49c72d298c5651583dfc608d4796beee18
samgomena <samiam4612@gmail.com>
2019-02-09 13:53:48 -0800

    feat: Add finding love utils and sky js logic

---
+++ scripts/sky.js
@@ -0,0 +1,44 @@
+class Sky {
+    constructor() {
+        this.group = new THREE.Group();
+
+        Utils.loadShader('Sky').then(obj => {
+            let geom = new THREE.IcosahedronGeometry(100, 4);
+            this.mat = this.getShader(obj.vs, obj.fs);
+            let mesh = new THREE.Mesh(geom, this.mat);
+            this.group.add(mesh);
+
+            // ShaderUIL.push(this.mat);
+        });
+    }
+
+    getShader(vs, fs) {
+        let uniforms = {
+            time: {type: 'f', value: 0},
+            sat: {type: 'f', value: 0},
+            // color: {type: 'c', value: new THREE.Color(0x49BEAA)}
+            color: {type: 'c', value: new THREE.Color(0x7F3DBC)}
+        };
+
+        // https://threejs.org/docs/#api/en/materials/ShaderMaterial.uniforms
+        let shader = new THREE.ShaderMaterial({
+            vertexShader: vs,
+            fragmentShader: fs,
+            uniforms
+        });
+
+        shader.side = THREE.BackSide;
+
+        return shader;
+    }
+
+    update() {
+        if (this.mat) {
+            this.mat.uniforms.time.value = performance.now() * 0.000010;
+            // let oldColor = this.mat.uniforms.color.value;
+            // Use this to convert to RGB and then enumerate the colors https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+            // Also looks useful https://htmlcolorcodes.com/
+            // this.mat.uniforms.color.value = new THREE.Color()
+        }
+    }
+}
