2e2dcbb6776bd84c3d97f3d2dc011636f2561fe4
samgomena <samgomena@protonmail.com>
2021-10-08 19:19:17 -0700

    feat: Analyze this bitch

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
@@ -9,4 +13,4 @@ module.exports = {
 
     return config;
   },
-};
+});
