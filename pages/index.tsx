a0bf3a71e210195ef8352a183d8d852c5f085d0b
samgomena <samgomena@protonmail.com>
2022-02-03 20:17:31 -0800

    feat: Format with prettier tailwind class sorting

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,12 @@ export default function Home() {
         <title>Sam Gomena</title>
       </Head>
 
-      <h1 className="text-7xl text-center">
+      <h1 className="text-center text-7xl">
         Hey, <br />
         I&apos;m Sam.
       </h1>
 
-      <p className="mt-6 text-xl text-center">
+      <p className="mt-6 text-center text-xl">
         Welcome to my little corner of the internet!
       </p>
     </div>
